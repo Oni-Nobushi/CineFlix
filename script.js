60c01f19fe52fd728e24914a6c908c63a4ad9ad7
Oni-Nobushi <francopoire77@gmail.com>
2024-12-03 13:59:43 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   contact.html
    	modified:   script.js

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ console.log("¡El archivo script.js está conectado correctamente!");
 document.addEventListener("DOMContentLoaded", () => {
   console.log("¡El DOM está completamente cargado!");
 });
+
+// Función para verificar si todos los campos del formulario están completos
+function verificarCamposFormulario() {
+  const nombre = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const asunto = document.getElementById('subject').value;
+  const mensaje = document.getElementById('message').value;
+
+  if (nombre === '' || email === '' || asunto === '' || mensaje === '') {
+    console.log('Por favor, completa todos los campos del formulario.');
+  } else {
+    console.log('¡Formulario completo!');
+  }
+}
+
+// Seleccionar el formulario y agregar el evento de "submit"
+const formulario = document.querySelector('form');
+formulario.addEventListener('submit', function(evento) {
+  evento.preventDefault(); // Evita que se envíe el formulario hasta hacer la verificación
+  verificarCamposFormulario();
+});
