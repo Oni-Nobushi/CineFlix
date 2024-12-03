fbc57776c61151061aad05381a9d787176ffac8e
Oni-Nobushi <francopoire77@gmail.com>
2024-12-03 13:34:14 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   index.html
    	new file:   script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Mensaje de prueba para verificar conexión
+console.log("¡El archivo script.js está conectado correctamente!");
+
+// Ejemplo de manipulación básica del DOM
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("¡El DOM está completamente cargado!");
+});
